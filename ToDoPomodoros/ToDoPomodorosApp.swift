ff0855c8438e2c6acd1f0743670917f341a42d0d
Ahmed Sha'ban <a.sh.tahan@gmail.com>
2023-01-02 21:43:22 +0200

    Initial Commit

---
+++ ToDoPomodoros/ToDoPomodorosApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoPomodorosApp.swift
+//  ToDoPomodoros
+//
+//  Created by Ahmed Shaban on 01/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoPomodorosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            TodoListView()
+        }
+    }
+}
